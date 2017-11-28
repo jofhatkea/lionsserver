01df546df28c66027e8a36957730df6bedc163d1
Jonas Holbech <jh@jonasholbech.dk>
2017-11-28 11:49:10 +0100

    initial commit

---
+++ client/lions.js
@@ -0,0 +1,120 @@
+/* eslint-env es6 */
+fetch("http://localhost:3000/lions/").then(d => d.json()).then(data => {
+    data.forEach(addLion);
+});
+
+let template = document.getElementById("lionTemplate").content;
+let parent = document.getElementById("app");
+
+function test(){
+  fetch("http://localhost:3000/lions").then(d=>d.json()).then(updateLions)
+}
+function updateLions(lions) {
+    //TODO check existing, modify accordingly (delete/add/update?)
+    //use property lastUpdated
+  let htmlLions = document.querySelectorAll("article.lion");
+  htmlLions.forEach(lion=>{
+    //lion.dataset.id
+    const index = lions.findIndex(i => i.id == lion.dataset.id);
+    //console.log(index)
+    if(index<0){
+      parent.removeChild(lion);
+    } else {
+      if(lion.dataset.lastUpdated < lions[index].lastUpdated){
+        updateLion(lion);  
+      }
+    }
+  })
+}
+
+function addLion(lion) {
+    let clone = template.cloneNode(true);
+    clone.querySelector("h1").textContent = lion.name;
+    clone.querySelector("h2 span").textContent = lion.age;
+    clone.querySelector("h2 span:nth-child(2)").textContent = lion.gender;
+    clone.querySelector("p").textContent = lion.pride;
+    clone.querySelector(`article`).dataset.id = lion.id;
+    clone.querySelector(`article`).dataset.lastUpdated = lion.lastUpdated;
+    clone.querySelector('button.update').addEventListener('click', (e) => {
+        let elem = e.target.parentElement; //.dataset.id
+        document.querySelector("input[name=name]").value = elem.querySelector("h1").textContent;
+        document.querySelector("input[name=age]").value = elem.querySelector("h2 span").textContent;
+        document.querySelector(`input[value='${elem.querySelector("h2 span:nth-child(2)").textContent}']`).checked = true;
+        document.querySelector("textarea[name=pride]").value = elem.querySelector("p").textContent;
+        document.querySelector("form").setAttribute('action', `http://localhost:3000/lions/${elem.dataset.id}`);
+        document.querySelector("form").dataset.id = elem.dataset.id;
+        document.querySelector("input[value='Update']").classList.toggle("hidden");
+        document.querySelector("input[value='Save']").classList.toggle("hidden");
+    })
+    clone.querySelector('button.delete').addEventListener('click', (e) => {
+        fetch(`http://localhost:3000/lions/${e.target.parentElement.dataset.id}`, {
+                method: "delete",
+            })
+            .then(d => d.json())
+            .then(d => {
+                parent.removeChild(document.querySelector(`article.lion[data-id='${d.id}']`)) //skal bruge ' her, hvorfor??
+            });
+    });
+    parent.appendChild(clone);
+}
+let submitButton = document.querySelector("input[type='submit']");
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    let lion = {
+        name: document.querySelector("input[name=name]").value,
+        age: document.querySelector("input[name=age]").value,
+        gender: document.querySelector("input[name=gender]:checked").value,
+        pride: document.querySelector("textarea[name=pride]").value
+    }//TODO refractor into function getValues()
+    //TODO optimistic insert, rollback on error?
+    fetch("http://localhost:3000/lions", {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',//TODO charset not needed?
+            "Accept":"application/json"
+        },
+        body: JSON.stringify(lion)
+    }).then(d => d.json()).then(t => {
+        addLion(t);
+        clearForm();
+    });
+})
+
+function clearForm() {
+    document.querySelector("input[name=name]").value = '';
+    document.querySelector("input[name=age]").value = '';
+    document.querySelectorAll("input[name=gender]").forEach(i => i.checked = false);
+    document.querySelector("textarea[name=pride]").value = ''
+    document.querySelector("input[value='Update']").classList.add("hidden");
+    document.querySelector("input[value='Save']").classList.remove("hidden");
+}
+//Edit, update, put
+let updateButton = document.querySelector("input[value='Update']");
+updateButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    let lion = {
+        name: document.querySelector("input[name=name]").value,
+        age: document.querySelector("input[name=age]").value,
+        gender: document.querySelector("input[name=gender]:checked").value,
+        pride: document.querySelector("textarea[name=pride]").value
+    }
+    fetch(`http://localhost:3000/lions/${document.querySelector("form").dataset.id}`, {
+        method: "put",
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify(lion)
+    }).then(d => d.json()).then(t => {
+        updateLion(t);
+        clearForm();
+    });
+})
+
+function updateLion(lion) {
+    let el = document.querySelector(`article[data-id='${lion.id}']`);
+    el.querySelector("h1").textContent = lion.name;
+    el.querySelector("span").textContent = lion.age;
+    el.querySelector("span:nth-child(2)").textContent = lion.gender;
+    el.querySelector("p").textContent = lion.pride;
+    el.dataset.lastUpdated = lion.lastUpdated;
+}
---
+++ lionserver.js
@@ -0,0 +1,121 @@
+/* eslint-env node, es6 */
+/*
+{
+  "name": "Simba",
+  "id": 1,
+  "age": 3,
+  "pride": "the cool cats",
+  "gender": "male",
+  "lastUpdated": Date.now()
+}*/
+const express = require('express');
+const bodyParser = require('body-parser');
+var _ = require("lodash");
+//var fs = require('fs');
+
+let app = express();
+
+//the client folder is the frontend, content is automatically served if no route is present
+app.use(express.static("client"));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+//var cors = require("cors");//should do the same?
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");//OPTIONS
+  next();
+});
+
+//var indexHTML = fs.readFileSync(__dirname+"/lions.html", "utf-8")
+
+let lions = [{
+  "name": "Simba",
+  "id": 1,
+  "age": 3,
+  "pride": "the cool cats",
+  "gender": "male",
+  "lastUpdated":Date.now()
+}];
+let counter=lions.length;
+/*
+app.get('/', function(req, res){
+    res.send(indexHTML); 
+    
+});*/
+
+/*"GET /lions": {
+    "desc": "returns all lions",
+    "response": "200 application/json",
+    "data": [{}, {}, {}]
+  }*/
+app.get('/lions', function(req, res) {
+  // send back a json response
+  res.json(lions);
+});
+/*"GET /lions/:id": {
+    "desc": "returns one lion respresented by its id",
+    "response": "200 application/json",
+    "data": {}
+  },*/
+  
+app.get('/lions/:id(\\d+)', function(req, res) {//(\\d+) not needed, but it's cool
+  let lion = _.find(lions, {id: parseInt(req.params.id)});
+  //res.json(req.params.id)
+  res.json(lion);//TODO handle undefined?
+});
+
+
+//routes: http://expressjs.com/en/guide/routing.html
+app.get('/lions/:key/:id(\\d+)', function(req, res) {
+  console.log(req.params.key, req.params.id);//virker
+  res.json(req.params);
+});
+app.get('/lions/gender/:gender', function(req,res){
+  let result = lions.filter(l=>l.gender === req.params.gender);
+  res.json(result);
+});
+/*"POST /lions": {
+    "desc": "create and returns a new lion uisng the posted object as the lion",
+    "response": "201 application/json",
+    "data": {}
+  },*/
+app.post('/lions', function(req, res) {
+  let lion = req.body;
+  counter++;
+  lion.id=counter+"";
+  lion.lastUpdated=Date.now()
+  lions.push(lion);
+  // res.send converts to json as well
+  // but req.json will convert things like null and undefined to json too although its not valid
+  //http://expressjs.com/en/api.html#res.send
+  res.send(lion);//seems like it accepts only one param [todos, todo]
+});
+
+/*"PUT /lions/:id": {
+    "desc": "updates and returns the matching lion with the posted update object",
+    "response": "200 application/json",
+    "data": {}
+  },*/
+app.put('/lions/:id', function(req, res) {
+  //const lion = _.find(lions, {id: parseInt(req.params.id)});
+  const index = lions.findIndex(i => i.id == req.params.id);
+  lions[index]=req.body;
+  lions[index].lastUpdated=Date.now();
+  lions[index].id=req.params.id;
+  res.json(lions[index]);
+});
+/*"DELETE /lions/:id": {
+    "desc": "deletes and returns the matching lion",
+    "response": "200 application/json",
+    "data": {}
+  }*/
+app.delete('/lions/:id', function(req, res) {
+  const index = lions.findIndex(i => i.id == req.params.id);
+  if(index===-1){
+    res.send();//TODO unused og vil jeg hellere ha et objekt?
+  }
+  res.json(lions.splice(index,1)[0]);
+});
+// start server on port 3000
+app.listen(3000);
